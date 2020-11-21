22672c758804d22a2b0ad411f2aefca0c3dc989a
Travis Briggs <audiodude@gmail.com>
2020-11-20 20:35:41 -0800

    Finish basic devise setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def after_sign_in_path_for(_resource)
+    # app_dashboard_index_path
+  end
 end
