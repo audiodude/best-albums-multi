4f89033afd804bbc5e3caf6dce4312b7985fbfad
Travis Briggs <audiodude@gmail.com>
2020-11-29 11:49:02 -0800

    Update album table

--- db/migrate/20201121042120_create_albums.rb
+++ db/migrate/20201121042120_create_albums.rb
@@ -1,7 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration[6.0]
   def change
     create_table :albums do |t|
-      t.string :name
+      t.string :title
       t.date :date
 
       t.string :mbid
